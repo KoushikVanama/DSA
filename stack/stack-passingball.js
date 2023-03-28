13e173effde00c912a183a5dad5999d3ef735a03
SaiKoushik <koushik.vanama@gmail.com>
2023-03-28 21:15:09 +0530

    stack passing ball

--- stack/stack-passingball.js
+++ stack/stack-passingball.js
@@ -1,34 +1,37 @@
-class LinkedList {
+class Stack {
   constructor() {}
 
   passingBall(A, B, C) {
     let stack = [B];
-    for(let i =0;i<C.length;i++){
-        if(C[i]==0)
-            stack.pop();
-        else
-            stack.push(C[i]);
+    for (let i = 0; i < C.length; i++) {
+      if (C[i] == 0) stack.pop();
+      else stack.push(C[i]);
     }
     return stack.pop();
   }
 }
 
-
 const readline = require("readline");
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
-const ll = new LinkedList();
+const stack = new Stack();
 
 console.log("Sample input: 10 23 [86,63,60,0,47,0,99,9,0,0]");
-rl.question("", function(line){
-    let inputArgs = line.split(" ");
-    console.log(ll.passingBall(parseInt(inputArgs[0]), parseInt(inputArgs[1]), JSON.parse(inputArgs[2])));
-    rl.close();
+rl.question("", function (line) {
+  let inputArgs = line.split(" ");
+  console.log(
+    stack.passingBall(
+      parseInt(inputArgs[0]),
+      parseInt(inputArgs[1]),
+      JSON.parse(inputArgs[2])
+    )
+  );
+  rl.close();
 });
 
 rl.on("close", function () {
-    process.exit(0);
+  process.exit(0);
 });
